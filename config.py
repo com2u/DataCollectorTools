c6a69008139997200d08e04a59b9ab230462bd73
K0N5T1 <konsti.wohner@outlook.de>
2021-06-30 15:44:39 +0200

    Docker init

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ class Config:
 
     DEBUG = False
     FLASK_APP = 'app.py'
-    SECRET_KEY = str((json.load(open('.\client_secrets.json')))["SECRET_KEY"])
-    OIDC_CLIENT_SECRETS = '.\client_secrets.json'
+    SECRET_KEY = str((json.load(open('./client_secrets.json')))["SECRET_KEY"])
+    OIDC_CLIENT_SECRETS = './client_secrets.json'
     OIDC_ID_TOKEN_COOKIE_SECURE = False
-    OIDC_VALID_ISSUERS = [str((json.load(open('.\client_secrets.json')))["OIDC_VALID_ISSUERS"])]
+    OIDC_VALID_ISSUERS = [str((json.load(open('./client_secrets.json')))["OIDC_VALID_ISSUERS"])]
     OIDC_SCOPES = ['openid', 'email', 'profile']
     OIDC_INTROSPECTION_AUTH_METHOD = 'client_secret_post'
     OIDC_CLOCK_SKEW = 560

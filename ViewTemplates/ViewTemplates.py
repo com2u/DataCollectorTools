122d1ffb047503968bc9a61970f91d2568b472f6
K0N5T1 <konsti.wohner@outlook.de>
2021-07-01 18:11:06 +0200

    added view template backend

--- ViewTemplates/ViewTemplates.py
+++ ViewTemplates/ViewTemplates.py
@@ -1,20 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from db_actions import get_postgres_instance
 
 view_templates = Blueprint(
     'view_templates', __name__, url_prefix="/view_templates"
 )
 
-@view_templates.route("/", methods=["GET"])
+@view_templates.route("/", methods=["GET", "POST"])
 def get_all_templates():
-    pass
+    if request.method == 'GET':
+        database = get_postgres_instance(dbname="dbtools")
+        table_names = database.get_table_names()
+        if "viewfilter" in table_names:
+            return jsonify({"data": [dict(row) for row in database.get_table("viewfilter")]})
+    if request.method == 'POST':
+        return ""
 
-@view_templates.route("/id/<view_id>", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
-def get_by_view_id():
-    pass
-
-@view_templates.route("/name/<view_name>", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
-def get_by_view_name():
-    pass
-
-
-
+@view_templates.route("/delete/<id>", methods=["DELETE"])
+def delete_view_template(id):
+    database = get_postgres_instance(dbname="dbtools")
+    return jsonify(success=database.delet_view_template(id))

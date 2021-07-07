545e0548f47141293c2ed02dffde9bb8777eeeeb
K0N5T1 <konsti.wohner@outlook.de>
2021-07-07 12:16:55 +0200

    ensure table exists

--- ViewTemplates/ViewTemplates.py
+++ ViewTemplates/ViewTemplates.py
@@ -5,6 +5,22 @@ view_templates = Blueprint(
     'view_templates', __name__, url_prefix="/view_templates"
 )
 
+
+@view_templates.before_app_first_request
+def database_initialization():
+    database = get_postgres_instance(dbname="dbtools")
+    database.engine.execute("""
+    CREATE TABLE IF NOT EXISTS public.viewfilter
+    (
+    id SERIAL,
+    viewname text COLLATE pg_catalog."default",
+    defaultrowcount integer,
+    rowcountoptions integer[],
+    invisiblecolumns text[] COLLATE pg_catalog."default",
+    PRIMARY KEY (id))
+    """)
+
+
 @view_templates.route("/", methods=["GET", "POST"])
 def get_all_templates():
     if request.method == 'GET':
@@ -13,7 +29,32 @@ def get_all_templates():
         if "viewfilter" in table_names:
             return jsonify({"data": [dict(row) for row in database.get_table("viewfilter")]})
     if request.method == 'POST':
-        return ""
+        params = request.values.to_dict(flat=False)
+        #formating params to fit database connection string
+        for param in ['viewname', 'defaultrowcount']:
+            if param not in params:
+                params[param] = ''
+            elif param in params:
+                params[param] = params[param][0]
+        if 'rowcountoptions' not in params or len(params['rowcountoptions']) == 0:
+            params[param] = "{}"
+        elif param in params:
+            params['rowcountoptions'] = str({int(option) for option in params['rowcountoptions'] if option != ''})
+        if 'invisiblecolumns' not in params or len(params['invisiblecolumns']) == 0:
+            params[param] = "{}"
+        elif param in params:
+            params['invisiblecolumns'] = {str(column) for column in params['invisiblecolumns'] if column  != ''}
+            if len(params['invisiblecolumns']) == 0:
+                params['invisiblecolumns'] = "{}"
+            else:
+                 params['invisiblecolumns'] =  str(params['invisiblecolumns'])
+
+        
+        database = get_postgres_instance(dbname="dbtools")
+        id = database.engine.execute(
+            f"""INSERT INTO viewfilter (viewname, defaultrowcount, rowcountoptions, invisiblecolumns) VALUES('{params['viewname']}', {int(params['defaultrowcount'])}, '{params['rowcountoptions']}', '{params['invisiblecolumns']}') RETURNING id;""").fetchone()[0]
+        return jsonify(id=id)
+
 
 @view_templates.route("/delete/<id>", methods=["DELETE"])
 def delete_view_template(id):

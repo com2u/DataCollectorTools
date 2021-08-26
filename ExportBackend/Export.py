53cad2f194868fa39aa23d340efaae89ce490168
K0N5T1 <konsti.wohner@outlook.de>
2021-08-26 15:37:56 +0200

    New Process Page Backend

--- ExportBackend/Export.py
+++ ExportBackend/Export.py
@@ -18,167 +18,114 @@ process_interface = Blueprint(
     'export_interface', __name__, url_prefix="/export_interface")
 
 
+@process_interface.route("/", methods=["GET"])
+def process():
+    params = request.values.to_dict(flat=False)
+    if 'check' in params:
+        if 'download' in params['check']:
+            downloadData = BytesIO()
+            if 'downloadExcel' in params['check']:
+                downloadData = get_excel(params, downloadData)
+            if 'downloadCSV' in params['check']:
+                downloadData = get_csv(params, downloadData)
+            if 'downloadImages' in params['check']:
+                downloadData = get_pictures(params, downloadData)
+            if 'downloadDatabaseDump' in params['check']:
+                downloadData = get_dump(params, downloadData)
+        if 'export' in params['check']:
+            exportData = BytesIO()
+            if 'exportExcel' in params['check']:
+                exportData = get_excel(params, exportData)
+            if 'exportCSV' in params['check']:
+                exportData = get_csv(params, exportData)
+            if 'exportImages' in params['check']:
+                exportData = get_pictures(params, exportData)
+            if 'exportDatabaseDump' in params['check']:
+                exportData = get_dump(params, exportData)
+        if 'delete' in params:
+            if 'deleteImages' in params:
+                test = 'test'
+    return jsonify(params)
 
 
-def get_database_dump():
-    with open("parameters.json") as file:
-        data = json.load(file)
-    command = ["pg_dump", f'--dbname=postgresql://{data["postgres_user"]}:{data["postgres_pw"]}@{data["postgres_url"]}:5432/{data["postgres_db"]}', '--format=c']
-    output = subprocess.Popen(command, stdout=subprocess.PIPE)
-    return output.stdout.read()
-
-def get_pictures_7z():
+def get_csv(filter, memory_file=BytesIO()):
     database = db_actions.get_postgres_instance()
-    
+    with py7zr.SevenZipFile(memory_file, 'w') as zf:
+        for table_name in database.get_view_names():
+            data_list = [dict(row) for row in database.get_table(
+                table_name, filter=filter)]
+            if len(data_list) > 0:
+                df = pd.DataFrame(data_list)
+                df.columns = database.get_table_columns(table_name)
+                zf.writestr(df.to_csv().encode(), arcname=f"{table_name}.csv")
+    return memory_file
+
+
+def get_excel(filter, memory_file=BytesIO()):
+    database = db_actions.get_postgres_instance()
+    writer = pd.ExcelWriter(memory_file, engine='xlsxwriter')
+    for table_name in database.get_view_names():
+        data_list = [dict(row) for row in database.get_table(
+            table_name, filter=filter)]
+        if len(data_list) > 0:
+            df = pd.DataFrame(data_list)
+            df.columns = database.get_table_columns(table_name)
+            df.to_excel(writer, sheet_name=str(table_name))
+    writer.close()
+    return memory_file
+
+
+def get_pictures(filter, memory_file=BytesIO()):
+    database = db_actions.get_postgres_instance()
     pathes_to_pictures = database.get_table_column_values(
-        "trigger_image_links", "image1", filter=request.values.to_dict(flat=False))
+        "triggerview", "image1_link", filter=filter)
     if len(pathes_to_pictures) > 0:
         zip_uuid = f"{str(uuid.uuid4())}"
         with open(zip_uuid + '.txt', "w") as file:
             for picture in pathes_to_pictures:
                 file.write(picture + "\n")
-        subprocess.run(["7z", "a", "-spf", "-t7z", "-m0=LZMA2:d64k:fb32", "-ms=8m", "-mmt=32", "-mx=1" , zip_uuid + ".7z" , "@" + zip_uuid + ".txt"], stdout=subprocess.PIPE)
+        subprocess.run(["7z", "a", "-spf", "-t7z", "-m0=LZMA2:d64k:fb32", "-ms=8m", "-mmt=32",
+                       "-mx=1", zip_uuid + ".7z", "@" + zip_uuid + ".txt"], stdout=subprocess.PIPE)
         with open(zip_uuid + ".7z", 'rb') as fh:
-            memory_file = BytesIO(fh.read())
-        #removing temporary files
+            memory_file.write(fh.read())
+        # removing temporary files
         os.remove(zip_uuid + ".7z")
         os.remove(zip_uuid + ".txt")
         return memory_file
-    return ""
+    return memory_file
 
 
-def to_dict(row):
-    if row is None:
-        return None
-    rtn_dict = dict()
-    keys = row.__table__.columns.keys()
-    for key in keys:
-        rtn_dict[key] = getattr(row, key)
-    return rtn_dict
-
-
-@process_interface.route("/folder/csv")
-@oidc.require_login
-def export_folder_csv():
-    database = db_actions.get_postgres_instance()
-    id = time.strftime("%Y%m%d-%H%M%S")
-    with open("parameters.json") as file:
-        data = json.load(file)
-    folder_name = Path(str(data["export_path"]), id)
-    folder_name.mkdir(parents=True, exist_ok=True)
-
-    # creating a csv file for every table
-    for table_name in database.get_view_names():
-        data_list = [dict(row) for row in database.get_table(
-            table_name, filter=request.values.to_dict(flat=False))]
-        if len(data_list) > 0:
-            df = pd.DataFrame(data_list)
-            df.columns = database.get_table_columns(table_name)
-            df.to_csv(os.path.join(folder_name, f"{table_name}.csv"))
-    return jsonify(export_folder=str(folder_name))
-
-
-@process_interface.route("/folder/excel")
-@oidc.require_login
-def export_folder_excel():
-    database = db_actions.get_postgres_instance()
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    for table_name in database.get_view_names():
-        data_list = [dict(row) for row in database.get_table(
-            table_name, filter=request.values.to_dict(flat=False))]
-        if len(data_list) > 0:
-            df = pd.DataFrame(data_list)
-            df.columns = database.get_table_columns(table_name)
-            df.to_excel(writer, sheet_name=str(table_name))
-    writer.close()
-    output.seek(0)
-
-    id = time.strftime("%Y%m%d-%H%M%S")
-    with open("parameters.json") as file:
-        data = json.load(file)
-    folder_name = Path(str(data["export_path"]), id)
-    folder_name.mkdir(parents=True, exist_ok=True)
-    with open(f"{folder_name}\\Tables.xlsx", "wb") as outfile:
-        outfile.write(output.getbuffer())
-    return jsonify(export_folder=str(folder_name))
-
-
-@process_interface.route("/folder/pictures")
-@oidc.require_login
-def export_folder_pictures():
-    database = db_actions.get_postgres_instance()
-    id = time.strftime("%Y%m%d-%H%M%S")
-    with open("parameters.json") as file:
-        data = json.load(file)
-    folder_name = Path(str(data["export_path"]), id)
-    folder_name.mkdir(parents=True, exist_ok=True)
-    pathes_to_pictures = database.get_table_column_values(
-        "trigger_image_links", "image1", filter=request.values.to_dict(flat=False))
-    if len(pathes_to_pictures) > 0:
-        for file in pathes_to_pictures:
-            shutil.copy(file)
-        return jsonify(export_folder=str(folder_name))
-    return ""
-
-
-@process_interface.route("/download/csv")
-@oidc.require_login
-def download_csv():
-    database = db_actions.get_postgres_instance()
-    memory_file = BytesIO()
-    with py7zr.SevenZipFile(memory_file, 'w') as zf:
-        for table_name in database.get_view_names():
-            data_list = [dict(row) for row in database.get_table(
-                table_name, filter=request.values.to_dict(flat=False))]
-            if len(data_list) > 0:
-                df = pd.DataFrame(data_list)
-                df.columns = database.get_table_columns(table_name)
-                zf.writestr(df.to_csv().encode(), arcname=f"{table_name}.csv")     
-    memory_file.seek(0)
-    return send_file(memory_file, attachment_filename="testing.7z", as_attachment=True)
-
-
-@process_interface.route("/download/excel")
-@oidc.require_login
-def download_excel():
-    database = db_actions.get_postgres_instance()
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    for table_name in database.get_view_names():
-        data_list = [dict(row) for row in database.get_table(
-            table_name, filter=request.values.to_dict(flat=False))]
-        if len(data_list) > 0:
-            df = pd.DataFrame(data_list)
-            df.columns = database.get_table_columns(table_name)
-            df.to_excel(writer, sheet_name=str(table_name))
-    writer.close()
-    output.seek(0)
-    return send_file(output, attachment_filename="testing.xlsx", as_attachment=True)
-
-
-@process_interface.route("/download/pictures")
-@oidc.require_login
-def download_pictures():
-    pictures = get_pictures_7z()
-    if pictures != "":
-        return send_file(pictures, attachment_filename="pictures_testing.7z", as_attachment=True)
-    return jsonify(success=False)
-
-@process_interface.route("/dump")
-@oidc.require_login
-def dump_all():
-    memory_file = BytesIO()
+def get_dump(memory_file=BytesIO()):
     with py7zr.SevenZipFile(memory_file, 'w', password="Pzma9T2nvz04KK1A9CU7") as zf:
-        zf.writestr(data=get_database_dump(), arcname="dump.backup")
-    memory_file.seek(0)
-    return send_file(memory_file, attachment_filename="dump.7z", as_attachment=True)
+        with open("parameters.json") as file:
+            data = json.load(file)
+        command = [
+            "pg_dump", f'--dbname=postgresql://{data["postgres_user"]}:{data["postgres_pw"]}@{data["postgres_url"]}:5432/{data["postgres_db"]}', '--format=c']
+        output = subprocess.Popen(command, stdout=subprocess.PIPE)
+        database_dump = output.stdout.read()
+        zf.writestr(data=database_dump, arcname="dump.backup")
+    return memory_file
 
-@process_interface.route("/dump_with_pictures")
-@oidc.require_login
-def dump_with_pictures():
-    memory_file = get_pictures_7z()
-    with py7zr.SevenZipFile(memory_file, "a", password="Pzma9T2nvz04KK1A9CU7") as zf:
-        zf.writestr(data=get_database_dump(), arcname="dump.backup")
-    memory_file.seek(0)
-    return send_file(memory_file, attachment_filename="dump_with_pictures.7z", as_attachment=True)
+
+def delete_db(filter):
+    database = db_actions.get_postgres_instance()
+    return jsonify(database.delete_vision(filter=filter))
+
+
+def delete_pictures(filter):
+    database = db_actions.get_postgres_instance()
+    pathes_to_pictures = database.get_table_column_values(
+        "trigger_image_links", "image1", filter=filter)
+    missing_files = []
+    deleted_files = []
+    if len(pathes_to_pictures) > 0:
+        for picture in pathes_to_pictures:
+            picture = Path(picture)
+            if picture.exists():
+                picture.unlink()
+                deleted_files.append(picture)
+            else:
+                missing_files.append(picture)
+
+        return jsonify(success=True, deleted_files=len(deleted_files), missing_files=len(missing_files))
+    return jsonify(success=False)
--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def init_app():
         from ExportBackend.Export import process_interface
         from ExportFrontend.export_frontend import export
         from Config.db_config import config_interface
-        from Delete.Delete import delete_interface
         from ViewTemplates.ViewTemplates import view_templates
        
         
@@ -29,7 +28,6 @@ def init_app():
         app.register_blueprint(process_interface)
         app.register_blueprint(export)
         app.register_blueprint(config_interface)
-        app.register_blueprint(delete_interface)
         app.register_blueprint(view_templates)
 
         return app
